afdcf84c315edcbf9f1d57e71d520d0f2c7283dc
Abel Mitiku <abelmgetnet@gmail.com>
2022-05-28 00:57:00 +0300

    resolve path error for test

--- scripts/data_preProcessing.py
+++ scripts/data_preProcessing.py
@@ -6,11 +6,10 @@ import os
 sys.path.insert(0, '../scripts/')
 sys.path.insert(0, '../logs/')
 sys.path.append(os.path.abspath(os.path.join('..')))
-
 from log_help import App_Logger
 
 
-app_logger = App_Logger("../logs/data_preProcessing.log").get_app_logger()
+app_logger = App_Logger("logs/data_preProcessing.log").get_app_logger()
 
 
 class data_preProcessing_script:
@@ -18,7 +17,7 @@ class data_preProcessing_script:
     def __init__(self, df: pd.DataFrame) -> None:
         self.df = df
         self.logger = App_Logger(
-            "../logs/data_preProcessing.log").get_app_logger()
+            "logs/data_preProcessing.log").get_app_logger()
 
     def drop_duplicates(self) -> pd.DataFrame:
         droped = self.df[self.df.duplicated()].index
