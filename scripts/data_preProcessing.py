a46a806126e514bcf6e3003bf3bfcf05df26e17d
Abel Mitiku <abelmgetnet@gmail.com>
2022-05-27 21:50:10 +0300

    modified

--- scripts/data_preProcessing.py
+++ scripts/data_preProcessing.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.abspath(os.path.join('..')))
 from log_help import App_Logger
 
 
-app_logger = App_Logger("logs/data_preProcessing.log").get_app_logger()
+app_logger = App_Logger("../logs/data_preProcessing.log").get_app_logger()
 
 
 class data_preProcessing_script:
@@ -18,7 +18,7 @@ class data_preProcessing_script:
     def __init__(self, df: pd.DataFrame) -> None:
         self.df = df
         self.logger = App_Logger(
-            "logs/data_preProcessing.log").get_app_logger()
+            "../logs/data_preProcessing.log").get_app_logger()
 
     def drop_duplicates(self) -> pd.DataFrame:
         droped = self.df[self.df.duplicated()].index
