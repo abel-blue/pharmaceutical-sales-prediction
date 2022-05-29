ad72e3bd7eea4ebc8dadbea9c9303869a4ec8c0e
Abel Mitiku <abelmgetnet@gmail.com>
2022-05-29 06:24:34 +0300

    modified dashboard to include ```css styles```

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 
+from multiapp import MultiApp
+from applications import visualizations, viz_model
 import os
 import sys
 import streamlit as st
 
 sys.path.insert(0, './scripts')
-from applications import visualizations, viz_model
-from multiapp import MultiApp
+
 
 st.set_page_config(page_title="Rossmann Sales Predictions", layout="wide")
 
@@ -14,6 +15,9 @@ app = MultiApp()
 st.sidebar.markdown("""
 # Rossmann Sales Predictions
 """)
+
+with open('./scripts/web-css/styles.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 # Add all your application here
 app.add_app("visualizations", visualizations.app)
